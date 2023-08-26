c930e07914e169199aa9fd651a08ab8595b1c6c7
hanabuu <hanabusa.tatsuo0119@gmail.com>
2023-08-26 13:58:22 +0900

    publishをブラウザで任意にできるようにexpress導入

--- publisher.js
+++ publisher.js
@@ -2,13 +2,18 @@
  * publisher.js
  * MQTTにおける送信者
  */
-var mqtt = require('mqtt');
+const mqtt = require('mqtt');
 // var client = mqtt.connect('mqtt://mosquitto.org');
-var client = mqtt.connect({
+const client = mqtt.connect({
     host: 'localhost',
     port: 1883,
     clientId: 'mqtt.publisher',
 });
+
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+const express = require("express");
+// 読み込んだexpressモジュールを実体化してインスタンス
+const app = express();
 
 /**
  * brokerへ接続
@@ -17,8 +22,25 @@ client.on('connect', function(){
     console.log('publisher.connected.');
 });
 
-client.publish('G800001-a01', "aaaaa");
-client.publish('G800001-a02', "aaaaa");
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+const server = app.listen(3001, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+// publishする
+app.get("/api/publish/a01", function(req, res, next){
+    console.log("publish a01");
+    client.publish('G800001-a01', "aaaaa");
+});
+
+// publishする
+app.get("/api/publish/a02", function(req, res, next){
+    console.log("publish a02");
+    client.publish('G800001-a02', "bbbb");
+});
+
+// client.publish('G800001-a01', "aaaaa");
+// client.publish('G800001-a02', "aaaaa");
 
 /**
  * 10sごとに配信を行う
